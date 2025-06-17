c2ae09efb4d903539af57b2f6840f88d60fe1bed
Gerardo Soto <soto1187@gmail.com>
2025-06-16 19:45:24 -0700

    frontend-code refactor

--- authorfinder-app/frontend/src/pages/authorsPage.tsx
+++ authorfinder-app/frontend/src/pages/authorsPage.tsx
@@ -175,26 +175,10 @@ function AuthorsPage() {
 
     return (
 
-      // <div className="toast" role="alert" aria-live="assertive" aria-atomic="true">
-      //   <div className="toast-header">
-      //     {/* <img src="..." className="rounded me-2" alt="..."> */}
-      //     <strong className="me-auto">Bootstrap</strong>
-      //     <small>11 mins ago</small>
-      //     <button type="button" className="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
-      //   </div>
-      //   <div className="toast-body">
-      //     Hello, world! This is a toast message.
-      //   </div>
-      // </div>
-
-      // <div className="alert alert-primary" role="alert">
-      //   A simple primary alert—check it out!
-      // </div>
-
-
       <div className={` ${bgColor} text-primary-emphasis`}>
-        <div className="text-2xl">{icon}</div>
-        <p className="font-semibold">{message}</p>
+        <div className="row">
+          <p className="font-semibold"><span>{icon}</span> {message}</p>
+        </div>
       </div>
     );
   };
@@ -211,97 +195,91 @@ function AuthorsPage() {
 
   return (
     <>
+      <div className="col-12 mb-2">
+        <h2 className="mb-6 text-center">
+          Explore Authors
+        </h2>
+      </div>
 
-      <div className="">
-        {/* <div className="row"> */}
-        <div className="col-12 mb-2">
-          <h2 className="mb-6 text-center">
-            Explore Authors
-          </h2>
-        </div>
-        {/* </div> */}
+      <div className=" text-center">
+        <div className="row container justify-content-center">
+          <div className="col-8">
+            <form onSubmit={handleSearch} className="max-w-3xl mx-auto mb-8">
+              <div className="input-group mb-3">
 
-        <div className=" text-center">
-          <div className="row container justify-content-center">
-            <div className="col-8">
-              <form onSubmit={handleSearch} className="max-w-3xl mx-auto mb-8">
-                <div className="input-group mb-3">
+                <input
+                  type="text"
+                  value={searchText}
+                  onChange={(e) => setSearchText(e.target.value)}
+                  placeholder="Search author by name (e.g., 'Jane Austen')"
+                  className="form-control"
+                />
 
-                  <input
-                    type="text"
-                    value={searchText}
-                    onChange={(e) => setSearchText(e.target.value)}
-                    placeholder="Search author by name (e.g., 'Jane Austen')"
-                    className="form-control"
-                  />
-
-                  <button
-                    type="submit"
-                    className="btn btn-outline-secondary btn-sm"
-                    disabled={loading}
-                  >
-                    {loading ? 'Searching...' : <><Search size={20} /> Search</>}
-                  </button>
-                </div>
-              </form>
-            </div>
+                <button
+                  type="submit"
+                  className="btn btn-outline-secondary btn-sm"
+                  disabled={loading}
+                >
+                  {loading ? 'Searching...' : <><Search size={20} /> Search</>}
+                </button>
+              </div>
+            </form>
           </div>
         </div>
+      </div>
 
-        {showMessageBox && (
-          <MessageBox
-            message={message}
-            type={messageType}
-            onClose={() => handleMessageBoxAction('cancel')} // Allow closing info/error/success boxes
-          />
-        )}
+      {showMessageBox && (
+        <MessageBox
+          message={message}
+          type={messageType}
+          onClose={() => handleMessageBoxAction('cancel')} // Allow closing info/error/success boxes
+        />
+      )}
 
-        {searchResults.length > 0 && (
-          <div className="shadow-lg rounded-lg p-4 bg-white">
-            <h3 className="text-2xl font-bold text-gray-700 mb-4">Search Results</h3>
-            <ul className="list-group list-group-flush">
-              {searchResults.map((author) => (
-                <li key={author.key} className="list-group-item">
+      {searchResults.length > 0 && (
+        <div className="shadow-lg rounded-lg p-4 bg-white">
+          <h3 className="text-2xl font-bold text-gray-700 mb-4">Search Results</h3>
+          <ul className="list-group list-group-flush">
+            {searchResults.map((author) => (
+              <li key={author.key} className="list-group-item">
 
-                  <div className="d-flex w-100 justify-content-between">
-                    <Link to={"/authors/" + author.key} className="text-decoration-none text-dark">
-                      <div className="d-flex align-items-center gap-3">
-                        {/* <img
+                <div className="d-flex w-100 justify-content-between">
+                  <Link to={"/authors/" + author.key} className="text-decoration-none text-dark">
+                    <div className="d-flex align-items-center gap-3">
+                      {/* <img
                           // src={`https://covers.openlibrary.org/a/olid/${author.key}-M.jpg`}
                           src={`https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTOVriLPru6AjmM5u0mjgJA67XQfeM27a1gAA&s`}
 
                           alt={author.name}
                           className="img-fluid"
                         /> */}
-                        <h5 className="mb-1 font-semibold">{author.name}</h5>
-                      </div>
-                    </Link>
-
-
-                    <small>
-                      <button className="btn btn-outline-danger btn-md text-decoration-none">
-                        {/* <span className="bi bi-heart-fill"></span> */}
-                        <span className="bi bi-heart"></span>
-
-                      </button>
-                    </small>
-                  </div>
-
-                  <Link to={"/authors/" + author.key} className="text-decoration-none text-dark">
-
-                    {author.top_work && (
-                      <p >Top Work: {author.top_work}</p>
-                    )}
-
-                    <small>View details...</small>
+                      <h5 className="mb-1 font-semibold">{author.name}</h5>
+                    </div>
                   </Link>
-                </li>
-              ))}
-            </ul>
-          </div>
-        )}
-      </div>
 
+
+                  <small>
+                    <button className="btn btn-outline-danger btn-md text-decoration-none">
+                      {/* <span className="bi bi-heart-fill"></span> */}
+                      <span className="bi bi-heart"></span>
+
+                    </button>
+                  </small>
+                </div>
+
+                <Link to={"/authors/" + author.key} className="text-decoration-none text-dark">
+
+                  {author.top_work && (
+                    <p >Top Work: {author.top_work}</p>
+                  )}
+
+                  <small>View details...</small>
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
 
 
       {loading && <LoadingSpinner />}
